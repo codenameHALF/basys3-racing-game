a602763973759002488cdf3327b9b91af95708ae
philly <s256709@dtu.dk>
2026-06-10 12:44:27 +0200

    Not tested

--- src/main/scala/CollisionSystem.scala
+++ src/main/scala/CollisionSystem.scala
@@ -1,43 +1,62 @@
 import chisel3._
 import chisel3.util._
 
-class CollisionSystem extends Module {
+class CollisionSystem(borderX: Int, borderY: Int) extends Module {
   val io = IO(new Bundle {
     val collisionDetected = Output(Bool())
 
     val entityX = Input(10.W)
-    val enityY = Input(9.W)
+    val entityY = Input(9.W)
 
-    val viewBoxX = Input(10.W) //0 to 640
-    val viewBoxY = Input(9.W) //0 to 480
+    //val borderX = Input(10.W) //0 to 640
+    //val borderY = Input(9.W) //0 to 480
     
   })
 
-    // Read TileMap func
+    val collisionTileMap = VecInit(300, UInt(1.W))
+    val entityTileIndex = UInt(9.W)
 
-    // Compare func
+    //  getCollisionTileMap func or just logic outside func
+
+    
+/* Declare where and when intializing a Collision system for the player
+    io.entityX := GameLogic.io.spriteXPosition(PLAYER)
+    io.entityY := GameLogic.io.spriteYPosition(PLAYER)
+ */
 
     // FSM Vals
-    val idle :: collisionState :: getEntityTileIndexState :: compareState :: Nil = Enum(4)
+    val idle :: collisionDetectedState :: computeEntityTileIndexState :: compareState :: Nil = Enum(4)
     val collisionStateReg = RegInit(idle)
 
     switch(collisionStateReg){
         is(idle){
-            // go to getPlayerTimeIndexState
+            // Out of bounds detection
+            when(io.entityX <= borderX || io.entityX >= borderX || io.entityY <= borderY || io.entityY >= borderY){
+                io.ollisionStateReg := collisionState
+            }.otherwise{
+                io.collisionDetected := true.B   
+            }
+
+            collisionStateReg := computeEntityTileIndexState
         }
-        
-        is(collisionState){
-            collisionDetected := true.B
+        is(collisionDetectedState){
+            io.collisionDetected := true.B   
             collisionStateReg := idle
         }
 
-        is(getEntityTileIndexState){
-            
-            // go to compare
+        is(computeEntityTileIndexState){
+            // May not work lol
+            EntityTileIndex := (borderX + entityX)/32 + 40 * ((borderY + entityY)/32)
+            collisionStateReg := compareState
         }
-        
         is(compareState){
-            // return to idle or go to collision state
+           when(collisionTileMap(entityTileIndex) === 1){
+                collisionStateReg := collisionDetectedState
+            }.otherwise{
+                io.collisionDetected := false.B
+                collisionStateReg := idle
+            }
+
         }
         
     }
