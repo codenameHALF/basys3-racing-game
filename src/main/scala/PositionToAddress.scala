491a3de3631fa3864e9c4b65ed9bb5a3583add16
codenameHALF <halfdanborg@gmail.com>
2026-06-13 21:51:14 +0200

    Merge and minor fix

--- src/main/scala/PositionToAddress.scala
+++ src/main/scala/PositionToAddress.scala
@@ -12,6 +12,6 @@ class PositionToAddress extends Module {
     val address = Output(UInt(11.W))
   })
 
-  io.address := (((viewBoxX + posX) >> 5) + 40 * ((viewBoxY + posY) >> 5)).U(11.W)
+  io.address := (posX.asUInt >> 5) + 40.U * ((posY.asUInt) >> 5).U(11.W)
 
 }
